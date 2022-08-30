66d8124c43f749344cdff031629e052ca5a0ab6d
nemetsg <galn051218@gmail.com>
2022-08-30 20:45:43 +0300

    added com4 & com5 to client.cpp

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -14,9 +14,11 @@ int main(int argc, char** argv) {
     Upload_Files com1{"upload an unclassified csv file"};
     Algorithm_Settings com2{"algorithm settings"};
     Classify_Data com3{"classify data"};
+    Display_Results com4{"display results"};
+    Download_Results com5{"download results"};
     Display_Confusion_Matrix com6{"display confusion matrix"};
 
-    CLI cli = CLI(&com1, &com2, &com3, &com6);
+    CLI cli = CLI(&com1, &com2, &com3, &com4, &com5, &com6);
 
     cli.start(client);
 }
