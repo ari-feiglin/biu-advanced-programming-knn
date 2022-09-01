375a42b95e786d443c4d218f2bd100d1c6a9e867
nemetsg <galn051218@gmail.com>
2022-09-01 22:08:28 +0300

    added some token uses

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,4 +1,5 @@
-#include "cli.h"
+#include "knn.h"
+#include "serialization.h"
 
 using namespace streams;
 using namespace knn;
@@ -10,16 +11,52 @@ int main(int argc, char** argv) {
     }
 
     TCPSocket client = TCPSocket(argv[1], 0, argv[2], strtol(argv[3], NULL, 0));
- 
-    Upload_Files com1{"upload an unclassified csv file"};
-    Algorithm_Settings com2{"algorithm settings"};
-    Classify_Data com3{"classify data"};
-    Display_Results com4{"display results"};
-    Download_Results com5{"download results"};
-    Display_Confusion_Matrix com6{"display confusion matrix"};
+    
+    Serializer serializer;
+    serializer(&client);
 
-    CLI cli = CLI(&com1, &com2, &com3, &com4, &com5, &com6);
+    SerializationTokens token;
 
-    cli.start(client);
+    while (true) {
+        serializer >> token;
+
+        // just prints int
+        if (token == int_token) {
+            int print_int;
+            serializer >> print_int;
+            std::cout << print_int;
+            continue;
+        }
+
+        // just prints string
+        if (token == string_token) {
+            std::string print_string;
+            serializer >> print_string;
+            std::cout << print_string;
+            continue;
+        }
+
+        // sends something
+        if (token == send_token) {
+            serializer >> token;
+            
+            // sends int
+            if (token == int_token) {
+                int send_int;
+                std::cin >> send_int;
+                serializer << send_int;
+                continue;
+            }
+
+            // sends string
+            if (token == string_token) {
+                std::string send_string;
+                std::cin >> send_string;
+                serializer << send_string;
+                continue;
+            }
+        }
+    }
+
 }
 
